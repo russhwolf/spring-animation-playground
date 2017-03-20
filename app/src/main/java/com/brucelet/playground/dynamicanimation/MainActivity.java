25dbd403fa5464d71fe2f5f719306795899162a0
Russell <russhwolf@gmail.com>
2017-03-19 22:30:40 -0400

    Snap to pointer

--- app/src/main/java/com/brucelet/playground/dynamicanimation/MainActivity.java
+++ app/src/main/java/com/brucelet/playground/dynamicanimation/MainActivity.java
@@ -4,17 +4,23 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.animation.SpringAnimation;
 import android.view.MotionEvent;
-import android.view.VelocityTracker;
 import android.view.View;
 import android.widget.SeekBar;
 
 public class MainActivity extends Activity {
 
-    private float downX, downY;
     private SeekBar damping, stiffness;
     private SpringAnimation animX, animY;
-    private VelocityTracker velocityTracker;
-    private boolean isDragging;
+    private View root;
+    private View box;
+
+    private float getOffsetX() {
+        return (root.getWidth()) / 2;
+    }
+
+    private float getOffsetY() {
+        return (root.getHeight() + damping.getBottom()) / 2;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,55 +28,33 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         stiffness = (SeekBar) findViewById(R.id.stiffness);
         damping = (SeekBar) findViewById(R.id.damping);
-        velocityTracker = VelocityTracker.obtain();
-        final View box = findViewById(R.id.box);
-        findViewById(R.id.root).setOnTouchListener(new View.OnTouchListener() {
+        box = findViewById(R.id.box);
+        root = findViewById(R.id.root);
+        root.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
                 switch (event.getAction()) {
                     case MotionEvent.ACTION_DOWN:
-                        float x = event.getX();
-                        float y = event.getY();
-                        if (box.getLeft() < x && box.getRight() > x && box.getTop() < y && box.getBottom() > y) {
-                            isDragging = true;
-                            downX = event.getX();
-                            downY = event.getY();
-                            velocityTracker.addMovement(event);
-                        }
-                        return true;
                     case MotionEvent.ACTION_MOVE:
-                        if (isDragging) {
-                            box.setTranslationX(event.getX() - downX);
-                            box.setTranslationY(event.getY() - downY);
-                            velocityTracker.addMovement(event);
+                        if (animX == null || !animX.isRunning()) {
+                            animX = new SpringAnimation(box, SpringAnimation.TRANSLATION_X,
+                                                        event.getX() - getOffsetX());
+                            animX.getSpring().setStiffness(getStiffness());
+                            animX.getSpring().setDampingRatio(getDamping());
+                            animX.setStartVelocity(0);
+                            animX.start();
+                        } else {
+                            animX.getSpring().setFinalPosition(event.getX() - getOffsetX());
                         }
-                        return true;
-                    case MotionEvent.ACTION_UP:
-                    case MotionEvent.ACTION_CANCEL:
-                        if (isDragging) {
-                            velocityTracker.computeCurrentVelocity(1000);
-                            if (box.getTranslationX() != 0) {
-                                if (animX != null) {
-                                    animX.cancel();
-                                }
-                                animX = new SpringAnimation(box, SpringAnimation.TRANSLATION_X, 0);
-                                animX.getSpring().setStiffness(getStiffness());
-                                animX.getSpring().setDampingRatio(getDamping());
-                                animX.setStartVelocity(velocityTracker.getXVelocity());
-                                animX.start();
-                            }
-                            if (box.getTranslationY() != 0) {
-                                if (animY != null) {
-                                    animY.cancel();
-                                }
-                                animY = new SpringAnimation(box, SpringAnimation.TRANSLATION_Y, 0);
-                                animY.getSpring().setStiffness(getStiffness());
-                                animY.getSpring().setDampingRatio(getDamping());
-                                animY.setStartVelocity(velocityTracker.getYVelocity());
-                                animY.start();
-                            }
-                            velocityTracker.clear();
-                            isDragging = false;
+                        if (animY == null || !animY.isRunning()) {
+                            animY = new SpringAnimation(box, SpringAnimation.TRANSLATION_Y,
+                                                        event.getY() - getOffsetY());
+                            animY.getSpring().setStiffness(getStiffness());
+                            animY.getSpring().setDampingRatio(getDamping());
+                            animY.setStartVelocity(0);
+                            animY.start();
+                        } else {
+                            animY.getSpring().setFinalPosition(event.getY() - getOffsetY());
                         }
                         return true;
                 }
